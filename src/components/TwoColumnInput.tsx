998d70333b9e1c5a7e661d8ddfb0ba8c7af1327f
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-07-26 00:03:12 +0530

    Refactor the Design Elements

--- src/components/TwoColumnInput.tsx
+++ src/components/TwoColumnInput.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { TwoColumnInputProps } from "../types/components/props.types";
 
 const TwoColumnInput: React.FC<TwoColumnInputProps> = ({ children }) => {
-  return <section className="flex gap-10">{children}</section>;
+  return <section className="flex gap-5">{children}</section>;
 };
 
 export default TwoColumnInput;
